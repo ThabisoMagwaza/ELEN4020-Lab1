045916e993bae3de0f6f9272edc956ab1ed04be3
836403@students.wits.ac.za <836403@students.wits.ac.za>
2018-02-22 01:53:26 +0200

    complete lab1

--- lab1.c
+++ lab1.c
@@ -52,15 +52,6 @@ void tenPercentOnes(int* array, int dim[]){ //sets 10 percent of the array eleme
 	}
 }
 
-void getIndexAndValue(int* array, int dim[]){
-
-	//int fivePercent = size * 0.05; // 5 percent of the array
-	
-	
-	
-}
-
-
 
 int numDimensions(int dim[]){
 	int counter = 0;
@@ -71,32 +62,91 @@ int numDimensions(int dim[]){
 }
 
 
+
 int* makeRandIndex(int dim[]){ //return a randon co-ordinate in the bounds of 'dim'
-	clock_t t;
-	srand((unsigned) time(&t));
-	int size = numDimensions(dim) - 1;
+	//clock_t t;
+	//srand((unsigned) time(&t));
+	int size = numDimensions(dim);
 	int* temp = makeDynamicIntArray(size);
 
-	for(int i = 0;i < size - 1;i++){
+	for(int i = 0;i < size;i++){
 		temp[i] = rand()%dim[i];
 	}
 	
 	return temp;
 }
 
+
+
+int coordinateToIndex(int* coordinate,int dim[]){
+	int index = 0;
+	int dimensions = numDimensions(dim);
+	for(int i = 0;i < dimensions;i++){
+		int sum = coordinate[i];
+		for(int j = i+1;j<dimensions;j++){
+			sum = sum * dim[j];
+		}
+		index = index + sum;
+	}
+	
+	return index;
+}
+
+void getIndexAndValue(int* array, int dim[]){ //picks and random co-ordinate in the array and prints out the value contained in it
+	
+	int size = arraySize(dim);
+	int fivepercent = size * 0.05; //five percent of the array
+	
+	for(int i = 0;i < fivepercent ;i++){
+		int* coordinate = makeRandIndex(dim);
+		printf("Co-ordinate\n");
+		printArray(coordinate,numDimensions(dim));
+		printf("\n value\n");
+		int index =  coordinateToIndex(coordinate,dim);
+		printf("%d",array[index]);
+		printf("\n\n");
+		free(coordinate);
+	
+	}
+	
+	
+}
+
 int main(){
-	int dim[] = {3,6,0};
+	clock_t t;
+	srand((unsigned) time(&t));
 	
+	printf("----------- Array 1 --------- \n\n");
+	int dim[] = {100,100,0};
+	int* array = makeMultiDimensionalIntArray(dim);
+	initialize(array,dim); //method 1
+	tenPercentOnes(array,dim); // method2
+	getIndexAndValue(array,dim); //method 3
 	
-	//int* array = makeMultiDimensionalIntArray(dim);
-	//initialize(array,dim);
-	//tenPercentOnes(array,dim);
-	//printf("%d", array[0]);
-	int* indices = makeRandIndex(dim);
-	//printArray(array,arraySize(dim));
-	printArray(indices,numDimensions(dim));
+	printf("----------- Array 2 --------- \n\n");
+	int dim2[] = {100,100,100,0};
+	int* array2 = makeMultiDimensionalIntArray(dim2);
+	initialize(array2,dim2); //method 1
+	tenPercentOnes(array2,dim2); // method2
+	getIndexAndValue(array2,dim2); //method 3
 	
-	//free(array);
+	printf("----------- Array 3 --------- \n\n");
+	int dim3[] = {50,50,50,0};
+	int* array3 = makeMultiDimensionalIntArray(dim3);
+	initialize(array3,dim3); //method 1
+	tenPercentOnes(array3,dim3); // method2
+	getIndexAndValue(array3,dim3); //method 3
 	
+	printf("----------- Array 4 --------- \n\n");
+	int dim4[] = {20,20,20,20,0};
+	int* array4 = makeMultiDimensionalIntArray(dim4);
+	initialize(array4,dim4); //method 1
+	tenPercentOnes(array4,dim4); // method2
+	getIndexAndValue(array4,dim4); //method 3
+	
+	free(array);
+	free(array2);
+	free(array3);
+	free(array4);
 	return(0);
 }
